e192452c0a7cd1fbe601d9bc95caee98188e7b04
majsai <majsaiadam@gmail.com>
2013-09-20 22:45:44 +0200

    extended layout with menu

---
+++ js/labyrinth.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+	var $ = document.querySelector.bind(document);
+
+	var default_class = "hud-option";
+	var a = $("#menu-button").onclick=function(){
+		console.log("cucc");
+		$("#menu").className = default_class + " menu-active";
+		$("#table-mask").className = "mask-active";
+	};
+
+	$("#menu-exit").onclick=function(){
+		$("#menu").className = default_class + " menu-inactive";
+		$("#table-mask").className = "mask-inactive";
+	};
+};
